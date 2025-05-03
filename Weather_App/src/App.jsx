47257efc5aa2fcee3622ad1f10aa4ac15c0e2461
Ola_Samir <olasamirshawwal@gmail.com>
2025-05-03 17:50:48 +0300

    Add Weather_App

---
+++ Weather_App/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Weather from './Weather';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Simple Weather App</h1>
+      <Weather />
+    </div>
+  );
+}
+
+export default App;
